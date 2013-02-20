3a3ddee3312afc119cdd2584ab15509ef90502b9
Boris Gordon <boris@boztek.net>
2013-02-20 23:40:23 +1100

    Test boxen::project dir setting

--- modules/projects/manifests/drupal8.pp
+++ modules/projects/manifests/drupal8.pp
@@ -2,6 +2,7 @@ class projects::drupal8 {
   include mysql
 
   boxen::project { 'drupal8':
+    dir    => '/Users/boris/src/drupal8/public',
     mysql  => true,
     nginx  => true,
     source => 'boztek/drupal8'
